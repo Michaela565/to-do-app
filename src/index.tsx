836270f189ee951c5b4fa94aaf7cd8fc1266075c
Michaela565 <60190944+Michaela565@users.noreply.github.com>
2023-08-31 17:00:26 +0200

    make index.tsx compile correctly

--- src/index.tsx
+++ src/index.tsx
@@ -7,6 +7,11 @@ import { Diary } from "./diary";
 import Task from "./reactComponents/Task";
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { main } from "./page";
 
-main();
+const taskk = new ToDoTask("a coool taaaaaask", new Date("2023-09-02"), 2, true);
+
+const todolistElement : any = document.querySelector(".todolist-object");
+
+const tasksRoot = createRoot(todolistElement);
+
+//tasksRoot.render(<Task {...taskk}/>)
