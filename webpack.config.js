b7077e101f044cfe4dfde2fbc97096ae743f3bd7
Michaela565 <60190944+Michaela565@users.noreply.github.com>
2023-08-31 17:36:29 +0200

    revert to react branch

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-   entry: './src/index.js',
-   output: {
-      path: path.join(__dirname, '/dist'),
-      filename: 'bundle.js'
-   },
-   devServer: {
-      port: 8080
-   },
-   module: {
-      rules: [
-         {
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-         },
-         {
-            test: /\.css$/,
-            use: [ 'style-loader', 'css-loader' ]
-        }
-      ]
-   },
-   plugins:[
-       new HtmlWebpackPlugin({
-            template: path.join(__dirname,'/src/index.html')
-       }) 
-   ]
-}
+  mode: 'development',
+  entry: './src/index.tsx',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use:[
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/images/'
+            }
+          }
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+};
